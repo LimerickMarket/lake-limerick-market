f0a31796224134efa8084f83fa45f0dfe4b5f5a8
Zachary Jones <zjrubyhouse@gmail.com>
2025-11-04 17:28:19 -0800

    Disable SassC preprocessing for CSS files to support modern CSS syntax

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,10 @@ Rails.application.config.assets.version = "1.0"
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+# Don't process .css files with SassC - allows modern CSS syntax to pass through
+# This prevents SassC from trying to parse modern CSS features (like CSS Color Level 4)
+# that it doesn't understand, which would cause build failures
+Rails.application.config.assets.configure do |env|
+  env.unregister_preprocessor('text/css', Sprockets::SassCompressor)
+end
